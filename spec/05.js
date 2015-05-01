e9ac13b73a81f613fe247a1547da5925961257e1
N.Shimizu <chikoski@gmail.com>
2015-05-01 13:48:38 +0900

    fixed a typo in spec/05.js

--- spec/05.js
+++ spec/05.js
@@ -2,7 +2,7 @@ describe("two: zero と succ を利用した関数", function(){
   it("配列を返す", function(){
     expect(two()).to.be.an("array");
   });
-  it("返り値の長さは1", function(){
+  it("返り値の長さは2", function(){
     expect(two().length).to.be(2);
   });
 });
