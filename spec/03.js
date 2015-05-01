a9fab07114ca24a3f2003531891d4d0acd4b9567
N.Shimizu <chikoski@gmail.com>
2015-05-01 13:41:26 +0900

    added ex03

---
+++ spec/03.js
@@ -0,0 +1,20 @@
+describe("succ: 配列の長さを1増やす関数", function(){
+  context("長さ0の配列を引数に指定した場合", function(){
+    it("配列を返す", function(){
+      expect(succ([])).to.be.an("array");
+    });
+    it("返り値の長さは1", function(){
+      expect(succ([]).length).to.be(1);
+    });
+  });
+
+  context("長さn（nは1以上の自然数）の配列を引数に指定した場合", function(){
+    it("配列を返す", function(){
+      expect(succ([1, 2, 3, 4, 5])).to.be.an("array");
+    });
+    it("返り値の長さは、引数のそれよりも1大きい", function(){
+      expect(succ([1, 2, 3, 4, 5])).to.be(6);
+      expect(succ([1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12])).to.be(13);
+    });
+  });
+});
