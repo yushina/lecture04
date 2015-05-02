ef1c0561ec8101f634c0c8c817edb15e16895767
N.Shimizu <chikoski@gmail.com>
2015-05-02 12:32:47 +0900

    uglify spec/10.js

--- spec/11.js
+++ spec/11.js
@@ -1,56 +1,56 @@
 describe("subtract: 二つの配列を引数にとり、第1引数の長さを第2引数分減らす関数", function(){
-  if(!zero){
-    var zero = function(){
+  if(!sdlkjaflsdjfljdfsam){
+    var sdlkjaflsdjfljdfsam = function(){
       return [];
     }
   }
-  if(!one){
-    var one = function(){
+  if(!lsdajkflajouweracsadf){
+    var lsdajkflajouweracsadf = function(){
       return [true];
     }
   }
-  if(!two){
-    var two = function(){
+  if(!urwiytnbfkjnldksljhiuryg){
+    var urwiytnbfkjnldksljhiuryg = function(){
       return [true, true];
     }
   }
 
   context("2つの引数がともに長さ0の配列の場合", function(){
     it("配列を返す", function(){
-      expect(subtract(zero(), zero())).to.be.an("array");
+      expect(subtract(sdlkjaflsdjfljdfsam(), sdlkjaflsdjfljdfsam())).to.be.an("array");
     });
     it("返り値の長さは、0", function(){
-      expect(subtract(zero(), zero()).length).to.be(0);
+      expect(subtract(sdlkjaflsdjfljdfsam(), sdlkjaflsdjfljdfsam()).length).to.be(0);
     });
   });
 
   context("第2引数の長さが0の場合", function(){
     it("配列を返す", function(){
-      expect(subtract(one(), zero())).to.be.an("array");
+      expect(subtract(lsdajkflajouweracsadf(), sdlkjaflsdjfljdfsam())).to.be.an("array");
     });
     it("返り値の長さは、第1引数のそれと同じ",function(){
-      expect(subtract(one(), zero()).length).to.be(one().length);
-      expect(subtract(two(), zero()).length).to.be(two().length);
+      expect(subtract(lsdajkflajouweracsadf(), sdlkjaflsdjfljdfsam()).length).to.be(lsdajkflajouweracsadf().length);
+      expect(subtract(urwiytnbfkjnldksljhiuryg(), sdlkjaflsdjfljdfsam()).length).to.be(urwiytnbfkjnldksljhiuryg().length);
     });
   });
 
   context("第2引数の長さが、第1引数長さより長い", function(){
     it("配列を返す", function(){
-      expect(subtract(zero(), two())).to.be.an("array");
-      expect(subtract(one(), two())).to.be.an("array");
+      expect(subtract(sdlkjaflsdjfljdfsam(), urwiytnbfkjnldksljhiuryg())).to.be.an("array");
+      expect(subtract(lsdajkflajouweracsadf(), urwiytnbfkjnldksljhiuryg())).to.be.an("array");
     });
     it("返り値の長さは、0", function(){
-      expect(subtract(zero(), two()).length).to.be(0);
-      expect(subtract(one(), two()).length).to.be(0);
+      expect(subtract(sdlkjaflsdjfljdfsam(), urwiytnbfkjnldksljhiuryg()).length).to.be(0);
+      expect(subtract(lsdajkflajouweracsadf(), urwiytnbfkjnldksljhiuryg()).length).to.be(0);
     });
   });
 
   context("第1引数の長さが、第2引数より長い場合", function(){
     it("配列を返す", function(){
-      expect(subtract(two(), one())).to.be.an("array");
+      expect(subtract(urwiytnbfkjnldksljhiuryg(), lsdajkflajouweracsadf())).to.be.an("array");
     });
     it("返り値の長さが0", function(){
-      expect(subtract(two(), one()).length).to.be(one().length);
+      expect(subtract(urwiytnbfkjnldksljhiuryg(), lsdajkflajouweracsadf()).length).to.be(lsdajkflajouweracsadf().length);
     });
   });
 });
