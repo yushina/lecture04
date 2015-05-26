8f9cd02fb0d5ec2a09b2f0767db6f75ad3062543
yushina <s.g.wakita.model@gmail.com>
2015-05-26 19:28:09 +0900

    練習問題８を再びやりました

--- js/08.js
+++ js/08.js
@@ -1,14 +1,9 @@
 function n (a){
-   return [];
-}
-
-function n (a){
-   return new Array(a);
-}
-
-function n(){
-    if(a<0)
-         return null;
-    else.
-         return new Array(Number(a))
+   if (a == 0){
+       return new Array(a);
+   } else if (a >= 1){
+       return new Array(a);
+   } else if (a < 0){
+       return null;
+   }
 }
